2c1fc85c6e34dbdb7ac5fdce30d40b7936bc4480
Pavax <patrick.dobler@gmail.com>
2021-10-30 14:08:17 +0200

    cleanup

--- include/credentials_example.h
+++
@@ -1,6 +0,0 @@
-/*
-* CHANGME: Rename this file to credentials.h
-*/
-const char *devEui = ""; // CHANGEME: set to TTN Device EUI [MSB-Format]
-const char *appEui = ""; // CHANGEME set TTN Application EUI [MSB-Format]
-const char *appKey = ""; // CHANGEME set TTN Application Key [MSB-Format]
